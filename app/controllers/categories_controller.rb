b2e9ce2453755c3c1621e0490a971cd93fbcd8d4
Sheetal <shepleasure@gmail.com>
2016-03-30 18:57:32 +0530

    designed categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,19 +2,18 @@ class CategoriesController < ApplicationController
 
 	def index
 		@categories = Category.all
-		@baby = @categories[0]
-		@book = @categories[1]
-		@camera = @categories[2]
-		@costume = @categories[3]
+		@book = @categories[0]
+		@camera = @categories[1]
+		@clothing = @categories[2]
+		@computer = @categories[3]
 		@electronic = @categories[4]
 		@event = @categories[5]
-		@media = @categories[6]
-		@medical = @categories[7]
-		@musical = @categories[8]
-		@realestate = @categories[9]
-		@service = @categories[10]
-		@sport = @categories[11]
-		@vehicle = @categories[12]
+		@medical = @categories[6]
+		@musical = @categories[7]
+		@realestate = @categories[8]
+		@service = @categories[9]
+		@sport = @categories[10]
+		@vehicle = @categories[11]
 
 		@listings = Listing.joins(:reviews, :listing_attachments).select("listings.*, avg(reviews.rating) as average_rating").group("listings.id").order("average_rating DESC").limit(3)
 	end
@@ -33,18 +32,17 @@ class CategoriesController < ApplicationController
 		@category = Category.find(params[:id])
 
 		@categories = Category.all
-		@baby = @categories[0]
-		@book = @categories[1]
-		@camera = @categories[2]
-		@costume = @categories[3]
+		@book = @categories[0]
+		@camera = @categories[1]
+		@clothing = @categories[2]
+		@computer = @categories[3]
 		@electronic = @categories[4]
 		@event = @categories[5]
-		@media = @categories[6]
-		@medical = @categories[7]
-		@musical = @categories[8]
-		@realestate = @categories[9]
-		@service = @categories[10]
-		@sport = @categories[11]
-		@vehicle = @categories[12]
+		@medical = @categories[6]
+		@musical = @categories[7]
+		@realestate = @categories[8]
+		@service = @categories[9]
+		@sport = @categories[10]
+		@vehicle = @categories[11]
 	end
 end
